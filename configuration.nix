8ff3397916f3658b4b3ae21d20300b1eae5b3eff
phyxl <dzikrynp14@gmail.com>
2026-07-05 00:52:01 +0700

    mini.nvim

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,8 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.systemd-boot.configurationLimit = 10;
+  # boot.loader.grub.configurationLimit = 10;
 
   networking.hostName = "nixos";
   networking.firewall.enable = true;
--- nvim/lua/pack.lua
+++ nvim/lua/pack.lua
@@ -3,4 +3,22 @@ local cb = function(x) return "https://codeberg.org/" .. x end
 
 vim.pack.add({
     { src = gh("ayu-theme/ayu-vim") },
+    { src = gh("nvim-mini/mini.nvim") },
 })
+
+require("mini.files").setup({
+    mappings = {
+        go_in = "<CR>",
+        go_in_plus = "L",
+        go_out = "_",
+        go_out_plus = "H",
+    },
+    windows = {
+        preview = true,
+    },
+})
+
+vim.keymap.set("n", "<leader>e", "<cmd>lua MiniFiles.open()<CR>", { desc = "Toggle mini file explorer" })
+vim.keymap.set("n", "<leader>E", function()
+    MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+end, { desc = "Toggle mini file explorer to current opened file" })
