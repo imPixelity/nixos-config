bc0fe77d1ac7a9b7613d706d6fb69df04b8e67ff
phyxl <dzikrynp14@gmail.com>
2026-07-12 07:57:07 +0700

    stuff

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -87,6 +87,11 @@
     jack.enable = true;
   };
 
+  services.mysql = {
+    enable = true;
+    package = pkgs.mysql84;
+  };
+
   # Lenovo Thinkpad F4 Microphone LED problem
   services.udev.extraRules = ''
     SUBSYSTEM=="leds", KERNEL=="platform::micmute", RUN+="${pkgs.coreutils}/bin/chmod 666 /sys/class/leds/platform::micmute/brightness"
